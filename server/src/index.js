3c279a41d036a2c4a8c706a3abfb4e424256e649
shamssaniat <shams.saniat@northsouth.edu>
2025-04-26 12:28:12 +0600

    Login a user

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,12 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { verify } = require("jsonwebtoken");
 const { hash, compare } = require("bcryptjs");
+const {
+  createAccessToken,
+  createRefreshToken,
+  sendAccessToken,
+  sendRefreshToken,
+} = require("./tokens.js");
 
 const { fakeDB } = require("./fakeDB.js");
 
@@ -29,10 +35,6 @@ server.use(
 server.use(express.json()); // to support JSON-encoded bodies
 server.use(express.urlencoded({ extended: true })); //support URL-encoded bodies
 
-server.listen(process.env.PORT, () =>
-  console.log(`Server listening on port ${process.env.PORT}`)
-);
-
 // 1. Register a user
 server.post("/register", async (req, res) => {
   const { email, password } = req.body;
@@ -56,3 +58,35 @@ server.post("/register", async (req, res) => {
     });
   }
 });
+
+// 2. Login a user
+server.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // 1. Find user in "database". If not exist, send error
+    const user = fakeDB.find((user) => user.email === email);
+    if (!user) throw new Error("User does not exist");
+    // 2. Compare crypted password and see if it checks out. Send error if not
+    const valid = await compare(password, user.password);
+    if (!valid) throw new Error("Password not correct");
+    // 3. Create refresh and access token
+    const accesstoken = createAccessToken(user.id);
+    const refreshtoken = createRefreshToken(user.id);
+    // 4. Put the refreshtoken in the "database"
+    user.refreshtoken = refreshtoken;
+    console.log(fakeDB);
+    // 5. Send token. Refreshtoken as a cookie and accesstoken as a regular response
+    sendRefreshToken(res, refreshtoken);
+    sendAccessToken(res, req, accesstoken);
+  } catch (err) {
+    res.send({
+      error: `${err.message}`,
+      
+    })
+  }
+});
+
+server.listen(process.env.PORT, () =>
+  console.log(`Server listening on port ${process.env.PORT}`)
+);
